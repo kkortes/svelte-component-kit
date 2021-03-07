52fe6573f37c643916be99c83aa374c95978eb9c
kkortes <kim@kortes.se>
2021-03-07 22:57:01 +0100

    feature: added a Code component

---
+++ Code.svelte
@@ -0,0 +1,14 @@
+<code>
+  <slot />
+</code>
+
+<style>
+  code {
+    font-family: Roboto, sans-serif;
+    background:#fafafa;
+    border: 1px solid lightgray;
+    border-radius: 3px;
+    color: darkred;
+    padding: 0 4px;
+  }
+</style>
