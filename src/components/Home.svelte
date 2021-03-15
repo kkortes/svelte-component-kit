633a59e38446904734a3537ef5c37415742ca033
kkortes <kim@kortes.se>
2021-03-15 09:23:23 +0100

    small fix

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -10,9 +10,20 @@
   </a>
 </div>
 
-This website showcases my <Code>svelte-component-kit</Code>-repo on github.<br
-/><br />
+This website showcases my
+<span>svelte-component-kit</span>-repo on github.<br /><br />
 I'll be adding components to this kit over time.<br /><br />
 The goal is to provide clean and easy-to-use components for elements and layouts
 that are commonly used.<br /><br />
 Feel free to provide feedback, I'd love to hear your opinion on the code!
+
+<style>
+  span {
+    font-family: "Arial", serif;
+    letter-spacing: 1px;
+    background: #fafafa;
+    padding: 0 4px;
+    margin: 0 -4px;
+    color: #b32c00;
+  }
+</style>
