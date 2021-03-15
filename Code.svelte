be749b27817098d06338f6f5aa2d68a6fe949080
kkortes <kim@kortes.se>
2021-03-16 00:43:09 +0100

    feature: content of code component is now reactive

--- Code.svelte
+++ Code.svelte
@@ -1,19 +1,26 @@
 <script>
-  import { highlight, languages } from "prismjs";
+  import Prism from "prismjs";
   import "prism-svelte";
+
+  Prism.manual = true;
 
   let html;
 </script>
 
-<div contenteditable="true" bind:textContent={html} style="display: none;">
+<div
+  contenteditable="true"
+  on:DOMCharacterDataModified={({ target: { textContent } }) => {
+    html = textContent;
+  }}
+  bind:textContent={html}
+  style="display: none;"
+>
   <slot />
 </div>
 
 {#if html}
   <pre
     class="language-svelte">
-    <code class="language-svelte">
-      {@html highlight(html, languages.svelte, "svelte")}
-    </code>
+      {@html Prism.highlight(html, Prism.languages.svelte, 'svelte')}
   </pre>
 {/if}
