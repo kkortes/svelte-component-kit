c08ca643fc8ba437c79ec7081660382d3d617c61
kkortes <kim@kortes.se>
2021-03-15 08:36:17 +0100

    updated

--- Code.svelte
+++ Code.svelte
@@ -1,14 +1,31 @@
-<code>
+<script>
+  import Prism from "prismjs";
+  import "prism-svelte";
+
+  let html;
+
+  $: source = html;
+</script>
+
+<div contenteditable="true" bind:textContent={html} style="display: none;">
   <slot />
-</code>
+</div>
+
+{#if source}
+  <pre>
+    <code class="language-svelte">
+      {@html Prism.highlight(source, Prism.languages.svelte, "svelte")}
+    </code>
+  </pre>
+{/if}
 
 <style>
-  code {
+  /* code {
     font-family: Roboto, sans-serif;
     background: #fafafa;
     border: 1px solid lightgray;
     border-radius: 3px;
     color: darkred;
     padding: 0 4px;
-  }
+  } */
 </style>
