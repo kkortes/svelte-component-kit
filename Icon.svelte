505363c48f93b461884a0485461543a5fbeefcc4
kkortes <kim@kortes.se>
2021-06-13 13:58:39 +0200

    release 1.1.8

--- Icon.svelte
+++ Icon.svelte
@@ -4,17 +4,18 @@
   export let color = "";
 </script>
 
-<span class={`icon icon-${name}`} style={`font-size: ${size}px; --color: ${color};`}><span class="path1" /><span class="path2" /></span>
+<span class:color class={`icon icon-${name}`} style={`font-size: ${size}px; --color: ${color};`}><span class="path1" /><span class="path2" /></span>
 
 <style>
   .icon {
     position: relative;
     display: inline-block;
     text-shadow: 0.5px 0.5px 0.5px rgba(0,0,0,.35);
-    color: var(--color, #000);
   }
-  span:before {
-    color: var(--color, #000);
+  .color,
+  .color:before,
+  .color span:before {
+    color: var(--color, #000)
   }
   .icon span:nth-child(2) {
     text-shadow: none;
