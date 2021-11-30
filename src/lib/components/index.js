3aa67ff3d05701abaaba86c285fd3411cc312497
kkortes <kim@kortes.se>
2021-11-30 11:03:31 +0100

    release 1.2.7

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -1,6 +1,6 @@
-export { default as Cards } from './Cards.svelte';
-export { default as Code } from './Code.svelte';
-export { default as Crow } from './Crow.svelte';
-export { default as Hexagon } from './Hexagon.svelte';
-export { default as Icon } from './Icon.svelte';
-export { default as Tooltip } from './Tooltip.svelte';
+export { default as Cards } from '$lib/components/Cards.svelte';
+export { default as Code } from '$lib/components/Code.svelte';
+export { default as Crow } from '$lib/components/Crow.svelte';
+export { default as Hexagon } from '$lib/components/Hexagon.svelte';
+export { default as Icon } from '$lib/components/Icon.svelte';
+export { default as Tooltip } from '$lib/components/Tooltip.svelte';
